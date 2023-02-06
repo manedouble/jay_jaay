e967f861ccf9f486e67f047fa0bd85e385417670
manedouble <122263435+manedouble@users.noreply.github.com>
2023-02-06 19:28:57 +0900

    Add files via upload

---
+++ _03_Operator3.java
@@ -0,0 +1,21 @@
+package chap_02;
+
+public class _03_Operator3 {
+
+    public static void main(String[] args) {
+        // 비교 연산자
+
+        System.out.println(5 > 3); // 참이면 true, 거짓이면 false
+        System.out.println(5 >= 3); // 5는 3 이상
+        System.out.println(5 >= 5);
+        System.out.println(5 >= 7);
+
+        System.out.println(5 < 3);
+        System.out.println(5 <= 3);
+
+        System.out.println(5 == 3);
+        System.out.println(5 == 5);
+        System.out.println(5 != 5);
+        System.out.println(5 != 3);
+    }
+}
