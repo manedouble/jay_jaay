04a357551f95fea92dcc0e06fbc3e24a4481b08c
manedouble <122263435+manedouble@users.noreply.github.com>
2023-03-15 13:25:13 +0900

    Add files via upload

---
+++ newbie coding/chapter9-1/_01_Generics.java
@@ -0,0 +1,50 @@
+package chap_09;
+
+public class _01_Generics {
+    public static void main(String[] args) {
+        // 제네릭스
+        Integer[] iArray = {1, 2, 3, 4, 5};
+        Double[] dArray = {1.0, 2.0, 3.0, 4.0, 5.0};
+        String[] sArray = {"A", "B", "C", "D", "E"};
+        
+        printIntArray(iArray);
+                printDoubleArray(dArray);
+                printStringArray(sArray);
+        System.out.println("--------");
+
+        printAnyArray(iArray);
+        printAnyArray(dArray);
+        printAnyArray(sArray);
+    }
+
+
+    // T - type
+    private static <T> void printAnyArray(T[] array) {
+        for (T t : array) {
+            System.out.println(t + " ");
+        }
+        System.out.println();
+    }
+
+    private static void printStringArray(String[] sArray) {
+        for (String s : sArray) {
+            System.out.println(s + " ");
+        }
+        System.out.println();
+    }
+
+    private static void printDoubleArray(Double[] dArray) {
+        for (double d : dArray) {
+            System.out.println(d + " ");
+        }
+        System.out.println();
+    }
+
+    private static void printIntArray(Integer[] iArray) {
+        for (int i : iArray) {
+            System.out.println(i + " "); // 1 2 3 4 5
+        }
+        System.out.println();
+    }
+
+}
